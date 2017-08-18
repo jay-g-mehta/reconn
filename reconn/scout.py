9f1b9ad1aeb873dd79343a6ef3668855561fe43b
Jay Mehta <jay87.mehta@gmail.com>
2017-08-18 15:00:16 -0700

    Implemented configurable survey patterns and reconn timeout feature
    1. Support for defining survey groups(set of patterns) to reconn
       in file and log when pattern matches
    2. Support to terminate reconn when end reconn pattern is matched
    3. Support to terminate reconn when timeout happens, using SIGALRM.
       RECONN is no longer running forver
    4. reconn is now readline instead of read 4096 bytes

--- reconn/scout.py
+++ reconn/scout.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import signal
 import io
 import time
 import threading
@@ -17,6 +19,9 @@ CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
 file_lock = threading.Lock()
+survey_pattern_re_objs = None
+end_reconn = False
+
 
 class FileEventHandler(watchdog.events.FileSystemEventHandler):
     '''Define handlers for any filesystem events for a  given file'''
@@ -36,20 +41,21 @@ class FileEventHandler(watchdog.events.FileSystemEventHandler):
                  event.event_type,
                  event.is_directory,
                  event.src_path)
-        #LOG.debug("%s %s", threading.current_thread().ident,
-        #          threading.current_thread().name)
+        # LOG.debug("%s %s", threading.current_thread().ident,
+        #           threading.current_thread().name)
 
     def on_modified(self, event):
-        #LOG.debug("%s %s", threading.current_thread().ident,
-        #          threading.current_thread().name)
+        # LOG.debug("%s %s", threading.current_thread().ident,
+        #           threading.current_thread().name)
 
         # NOTE(jay):
         # Any processing on any handler function blocks
         # delivery/invoking of other handler function
-        #time.sleep(5)
-
+        # time.sleep(5)
+        global end_reconn
         if self._event_on_file_path(event):
-            lock_read_file(self._file)
+            if end_reconn is False:
+                lock_reconn_file(self._file)
 
 
 def register_notification(file_path, file_obj):
@@ -57,8 +63,6 @@ def register_notification(file_path, file_obj):
     descriptor'''
 
     observer = watchdog.observers.Observer()
-    #LOG.info(observer)
-    #LOG.info(dir(observer))
     event_handler = FileEventHandler(file_path, file_obj)
     observer.schedule(event_handler, path=os.path.dirname(file_path),
                       recursive=False)
@@ -66,62 +70,137 @@ def register_notification(file_path, file_obj):
     return observer
 
 
-def read_file(f):
+last_line = ''
+
+
+def reconn_file(f):
+    global end_reconn
+    global survey_pattern_re_objs
+    global last_line
+
     eof = False
-    read_size_at_once = 4096
     while not eof:
-        read_bytes = f.read(read_size_at_once)
-        if read_bytes is None:
-            # f was in non blocking mode, no bytes available.
-            LOG.debug(" f was in non blocking mode, no bytes available.")
-            break
-        elif len(read_bytes) == 0:
+        line = f.readline()
+
+        if line == '':
             eof = True
-            return 0
+            continue
 
-        print read_bytes
-    return 1
+        if last_line != '':
+            line = last_line + line
+            last_line = ''
+
+        if line[-1] != '\n':
+            # readline returned due to EOF
+            # Because pattern is matched on each line, buffer
+            # read contents till \n is read
+            last_line = line
+        else:
+            # readline returned due to \n
+            matched_pattern = reconn_utils.search_patterns(survey_pattern_re_objs,
+                                                           line)
+            if reconn_utils.is_pattern_to_end_reconn(matched_pattern):
+                # End Reconn pattern matched
+                end_reconn = True
+                print line
+                return
+
+        print line
+
+    # Reconn last line for patterns:
+    if end_reconn is False and eof is True and last_line != '':
+
+        matched_pattern = reconn_utils.search_patterns(survey_pattern_re_objs,
+                                                       last_line)
+        if matched_pattern is not None:
+            # Some pattern matched. No longer to carry last_line's content.
+            last_line = ''
+
+        if reconn_utils.is_pattern_to_end_reconn(matched_pattern):
+            # End Reconn pattern matched
+            end_reconn = True
 
 
-def lock_read_file(f):
+def lock_reconn_file(f):
     '''Lock before reading file, avoid race between
     main thread and event handler.
     Main thread requires reading of file so that it reads existing
     content to avoid a situation of no more events on file.'''
     file_lock.acquire()
-    read_file(f)
+    reconn_file(f)
     file_lock.release()
 
 
-def read_forever(console_file, observer):
-    observer.start()
+def reconn_forever(console_file, observer):
+    global end_reconn
 
+    observer.start()
     # Wait for observer thread to start. Don't want to miss any events
     time.sleep(2)
 
-    lock_read_file(console_file)
+    # Case: when log file has all data in it and no more writes will happen,
+    # so main thread has to reconn once.
+    lock_reconn_file(console_file)
 
     # Allow ctrl+c to work:
     try:
         while True:
             time.sleep(1)
+
+            if end_reconn is True:
+                terminate_reconn(observer, console_file)
     except KeyboardInterrupt:
-        observer.stop()
+        terminate_reconn(observer, console_file)
 
     # Allow observer thread to run forever and so this process
     observer.join()
+
+
+class ReconnTimeout(object):
+    @staticmethod
+    def clear_timeout():
+        '''De-register to receive SIGALRM for timeouts'''
+        signal.alarm(0)
+
+    @staticmethod
+    def set_timeout(timeout):
+        '''Register to receive SIGALRM for timeout'''
+        signal.signal(signal.SIGALRM, ReconnTimeout.reconn_timeout_handler)
+        signal.alarm(timeout)
+
+    @staticmethod
+    def reconn_timeout_handler(signum, stack_frame):
+        global end_reconn
+        end_reconn = True
+        ReconnTimeout.clear_timeout()
+        LOG.info("Reconn timed out")
 
 
 def init_reconn():
     reconn_utils.register_reconn_opts()
     reconn_utils.oslo_logger_config_setup()
 
+    for survey_pattern_group in reconn_utils.get_reconn_survey_groups():
+        reconn_utils.register_reconn_survey_patterns(survey_pattern_group)
+
+
+def terminate_reconn(observer, console_file):
+    '''Reconn closure activities executed here.
+    Called when timeout or end reconn pattern matched.
+    Discontinue any more reconn on files.'''
+    observer.stop()
+    observer.join()
+    console_file.close()
+    sys.exit(0)
+
 
 def main():
+    global survey_pattern_re_objs
+
     init_reconn()
     LOG.info("console_path: %s", CONF.reconn.console_path)
     try:
-        console_file = io.open(CONF.reconn.console_path, 'rb')
+        console_file = io.open(CONF.reconn.console_path, 'rt', newline='\n')
     except (IOError, TypeError) as e:
         LOG.error("Failed to open console log file. Error: %s", e)
         LOG.info("Exiting")
@@ -131,11 +210,14 @@ def main():
         LOG.info("Exiting")
         exit(1)
 
+    survey_pattern_re_objs = reconn_utils.create_re_objs()
+
     observer = register_notification(CONF.reconn.console_path, console_file)
 
-    read_forever(console_file, observer)
+    # Set program terminate time out
+    ReconnTimeout.set_timeout(CONF.reconn.timeout * 60)
 
-    console_file.close()
+    reconn_forever(console_file, observer)
 
 
 
--- reconn/utils.py
+++ reconn/utils.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 from reconn import version
 
@@ -7,6 +8,7 @@ from oslo_log import log as logging
 
 
 CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
 
 
 def oslo_logger_config_setup():
@@ -30,7 +32,21 @@ def register_reconn_opts():
                    help='Absolute file path of console.log '
                         'of a VM instance, RECONN is supposed '
                         'to stream read and look out for VM '
-                        'boot up stage'), ]
+                        'boot up stage'),
+
+        cfg.IntOpt('timeout',
+                   default=20,
+                   help='terminate reconn after timeout minutes. '
+                        'Defaults to 20 minutes'),
+
+        cfg.StrOpt('end_reconn',
+                   default=None,
+                   help='A [CONFIG] group name that defines a '
+                        'parameter called "pattern" which is an '
+                        'regular expression that will be looked '
+                        'out in file. On encountering end reconn '
+                        'pattern, reconn will be stopped'),
+    ]
 
     reconn_opt_group = cfg.OptGroup(name='reconn',
                                     title='RECONN opts group')
@@ -38,5 +54,92 @@ def register_reconn_opts():
     CONF.register_group(reconn_opt_group)
     CONF.register_opts(reconn_opts, group=reconn_opt_group)
 
+    _register_reconn_survey_opts(reconn_opt_group)
+
     CONF.register_cli_opts(reconn_opts)
 
+
+def _register_reconn_survey_opts(reconn_opt_group):
+    '''Register reconn survey opts'''
+    reconn_survey_opts = [
+        cfg.StrOpt('survey_group',
+                   default=None,
+                   help='Survey pattern groups name'),
+    ]
+
+    reconn_opt_group = cfg.OptGroup(name='reconn',
+                                    title='RECONN opts group')
+
+    CONF.register_opts(reconn_survey_opts, group=reconn_opt_group)
+
+
+def register_reconn_survey_patterns(survey_pattern_group):
+    '''Dynamically register each survey pattern'''
+    reconn_survey_pattern_opts = [
+        cfg.StrOpt('pattern',
+                   default=None,
+                   help=''),
+        cfg.StrOpt('success',
+                   default=None,
+                   help=''),
+        cfg.StrOpt('failure',
+                   default=None,
+                   help=''),
+    ]
+
+    reconn_survey_opt_group = cfg.OptGroup(name=survey_pattern_group,
+                                           title='RECONN survey pattern:' +
+                                                 survey_pattern_group)
+
+    CONF.register_opts(reconn_survey_pattern_opts, group=reconn_survey_opt_group)
+
+
+def get_reconn_survey_groups():
+    group_list = CONF.reconn.survey_group.split(",")
+    for i in range(len(group_list)):
+        group_list[i] = group_list[i].strip(" ")
+    return group_list
+
+
+def create_re_objs():
+    '''Create python re pattern obj for each survey pattern'''
+    re_objs = []
+    for survey_group_name in get_reconn_survey_groups():
+        re_objs.append(re.compile(CONF.get(survey_group_name).pattern))
+    return re_objs
+
+
+def search_patterns(re_objs, line):
+    '''Returns first matched pattern in line or None'''
+    for re_obj in re_objs:
+        match_obj = re_obj.search(line)
+        if match_obj is None:
+            # pattern not in line
+            pass
+        else:
+            # pattern in line
+            LOG.info("Matched %s in line:%s", re_obj.pattern, line)
+            return re_obj.pattern
+    return None
+
+
+def search_end_reconn_pattern(line):
+    '''Search end reconn pattern in line'''
+    reconn_end_group_name = CONF.reconn.end_reconn
+    end_reconn_pattern = CONF.get(reconn_end_group_name).pattern
+    if re.search(end_reconn_pattern, line) is None:
+        return None
+    else:
+        LOG.info("Matched End Reconn pattern: %s in line: %s",
+                 end_reconn_pattern, line)
+        return end_reconn_pattern
+
+
+def is_pattern_to_end_reconn(pattern):
+    '''Match pattern with end reconn group's pattern'''
+    reconn_end_group_name = CONF.reconn.end_reconn
+    end_reconn_pattern = CONF.get(reconn_end_group_name).pattern
+    if end_reconn_pattern == pattern:
+        return True
+    else:
+        return False
